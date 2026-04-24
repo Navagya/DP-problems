625ca5227a6eb98222e9d95e750d5c5afe76adaf
Navagya Singh <2k23csai2313125@gmail.com>
2026-04-25 00:18:27 +0530

    tabulation

---
+++ ClimbStairs_tab.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class ClimbStairs_tab {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int dp [] = new int [n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println("Distinct ways:"+ dp[n]);
+    }
+}
